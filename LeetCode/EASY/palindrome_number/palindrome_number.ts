f2affa4eb91072b1de02d40ba1fdd6d7546419b0
Piush Bose <piushbasu@gmail.com>
2023-08-22 08:36:20 +0530

    Update palindrome_number.ts

--- LeetCode/EASY/palindrome_number/palindrome_number.ts
+++ LeetCode/EASY/palindrome_number/palindrome_number.ts
@@ -5,7 +5,7 @@ function isPalindrome(x: number): boolean {
     if (x < 0) return false;
     if (stringX.split("").every(n => n === stringX[0])) return true;
 
-    for(let i = 0; i < stringX.length; i++) {
+    for(let i = 0; i <= Math.floor(stringX.length); i++) {
         if(stringX[i] === stringX[stringX.length - (i + 1)]) continue;
         else return false;
     }
